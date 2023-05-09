Adding new configs. Fixing small bugs. Adding training epochs and time computation.

cem/data/chexpert_loader.py
@@ -60,7 +60,7 @@ def generate_data(
     def prev_concept_transform(sample):
         if isinstance(sample, list):
             sample = np.array(sample)
-        return np.where(np.logical_or(sample == 1, sample == 0), sample, 0.5)
+        return np.where(np.logical_or(sample == 1, sample == 0), sample, config.get("unk_value", 0.5))
     if sampling_percent != 1:
         # Do the subsampling
         if sampling_groups:
@@ -148,6 +148,7 @@ def generate_data(
         concept_transform=concept_transform,
         path_transform=lambda path: os.path.join(root_dir, path),
         label_transform=lambda x: float(x),
+        is_chexpert=True,
     )
     if not config.get("train_subsampling", 1) in [1, 0, None]:
         percent = config.get("train_subsampling", 1)
@@ -225,6 +226,7 @@ def generate_data(
         concept_transform=concept_transform,
         path_transform=lambda path: os.path.join(root_dir, path),
         label_transform=lambda x: float(x),
+        is_chexpert=True,
     )
     if test_idxs is not None:
         test_dl = torch.utils.data.DataLoader(

cem/data/derm_loader.py
@@ -34,6 +34,7 @@ class Derm7ptDataset(object):
         label_transform=None,
         concept_transform=None,
         label_key="diagnosis",
+        label_generating_fn=None,
     ):
         self.use_full_concepts = use_full_concepts
         self.concept_transform = concept_transform
@@ -48,8 +49,111 @@ class Derm7ptDataset(object):
         self.posible_concept_vals = {}
         self.concept_map = {}
         self.meta = pd.read_csv(os.path.join(base_dir, "meta", "meta.csv"))
+        # current_index = 0
+        # self.posible_concept_vals["TypicalPigmentNetwork"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["TypicalPigmentNetwork"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["TypicalPigmentNetwork"] = [current_index]
+        #     current_index += 1
+        # self.meta["TypicalPigmentNetwork"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "typical": 1, "atypical": unc_value}[row["pigment_network"]],
+        #     axis=1,
+        # )
+
+        # self.posible_concept_vals["AtypicalPigmentNetwork"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["AtypicalPigmentNetwork"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["AtypicalPigmentNetwork"] = [current_index]
+        #     current_index += 1
+        # self.meta["AtypicalPigmentNetwork"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "typical": unc_value, "atypical": 1}[row["pigment_network"]],
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["RegularStreaks"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["RegularStreaks"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["RegularStreaks"] = [current_index]
+        #     current_index += 1
+        # self.meta["RegularStreaks"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "regular": 1, "irregular": unc_value}[row["streaks"]],
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["IrregularStreaks"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["IrregularStreaks"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["IrregularStreaks"] = [current_index]
+        #     current_index += 1
+        # self.meta["IrregularStreaks"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "regular": unc_value, "irregular": 1}[row["streaks"]],
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["RegressionStructures"] = [0, 1]
+        # if self.use_full_concepts:
+        #     self.concept_map["RegressionStructures"] = [current_index, current_index + 1]
+        #     current_index += 2
+        # else:
+        #     self.concept_map["RegressionStructures"] = [current_index]
+        #     current_index += 1
+        # self.meta["RegressionStructures"] = self.meta.apply(
+        #     lambda row: (1-int(row["regression_structures"] == "absent")),
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["RegularDG"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["RegularDG"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["RegularDG"] = [current_index]
+        #     current_index += 1
+        # self.meta["RegularDG"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "regular": 1, "irregular": unc_value}[row["dots_and_globules"]],
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["IrregularDG"] = [0, 1, unc_value]
+        # if self.use_full_concepts:
+        #     self.concept_map["IrregularDG"] = [current_index, current_index + 1, current_index + 2]
+        #     current_index += 3
+        # else:
+        #     self.concept_map["IrregularDG"] = [current_index]
+        #     current_index += 1
+        # self.meta["IrregularDG"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "regular": unc_value, "irregular": 1}[row["dots_and_globules"]],
+        #     axis=1,
+        # )
+
+
+        # self.posible_concept_vals["BWV"] = [0, 1]
+        # if self.use_full_concepts:
+        #     self.concept_map["BWV"] = [current_index, current_index + 1]
+        #     current_index += 2
+        # else:
+        #     self.concept_map["BWV"] = [current_index]
+        #     current_index += 1
+        # self.meta["BWV"] = self.meta.apply(
+        #     lambda row: {"absent": 0, "present": 1}[row["blue_whitish_veil"]],
+        #     axis=1,
+        # )
+
         current_index = 0
-        self.posible_concept_vals["TypicalPigmentNetwork"] = [0, 1, unc_value]
+        self.posible_concept_vals["TypicalPigmentNetwork"] = [0, 1, 2]
         if self.use_full_concepts:
             self.concept_map["TypicalPigmentNetwork"] = [current_index, current_index + 1, current_index + 2]
             current_index += 3
@@ -57,24 +161,11 @@ class Derm7ptDataset(object):
             self.concept_map["TypicalPigmentNetwork"] = [current_index]
             current_index += 1
         self.meta["TypicalPigmentNetwork"] = self.meta.apply(
-            lambda row: {"absent": 0, "typical": 1, "atypical": unc_value}[row["pigment_network"]],
+            lambda row: {"absent": 0, "typical": 1, "atypical": 2}[row["pigment_network"]],
             axis=1,
         )
 
-        self.posible_concept_vals["AtypicalPigmentNetwork"] = [0, 1, unc_value]
-        if self.use_full_concepts:
-            self.concept_map["AtypicalPigmentNetwork"] = [current_index, current_index + 1, current_index + 2]
-            current_index += 3
-        else:
-            self.concept_map["AtypicalPigmentNetwork"] = [current_index]
-            current_index += 1
-        self.meta["AtypicalPigmentNetwork"] = self.meta.apply(
-            lambda row: {"absent": 0, "typical": unc_value, "atypical": 1}[row["pigment_network"]],
-            axis=1,
-        )
-
-
-        self.posible_concept_vals["RegularStreaks"] = [0, 1, unc_value]
+        self.posible_concept_vals["RegularStreaks"] = [0, 1, 2]
         if self.use_full_concepts:
             self.concept_map["RegularStreaks"] = [current_index, current_index + 1, current_index + 2]
             current_index += 3
@@ -82,38 +173,20 @@ class Derm7ptDataset(object):
             self.concept_map["RegularStreaks"] = [current_index]
             current_index += 1
         self.meta["RegularStreaks"] = self.meta.apply(
-            lambda row: {"absent": 0, "regular": 1, "irregular": unc_value}[row["streaks"]],
+            lambda row: {"absent": 0, "regular": 1, "irregular": 2}[row["streaks"]],
             axis=1,
         )
 
-
-        self.posible_concept_vals["IrregularStreaks"] = [0, 1, unc_value]
-        if self.use_full_concepts:
-            self.concept_map["IrregularStreaks"] = [current_index, current_index + 1, current_index + 2]
-            current_index += 3
-        else:
-            self.concept_map["IrregularStreaks"] = [current_index]
-            current_index += 1
-        self.meta["IrregularStreaks"] = self.meta.apply(
-            lambda row: {"absent": 0, "regular": unc_value, "irregular": 1}[row["streaks"]],
-            axis=1,
-        )
-
-
-        self.posible_concept_vals["RegressionStructures"] = [0, 1]
-        if self.use_full_concepts:
-            self.concept_map["RegressionStructures"] = [current_index, current_index + 1]
-            current_index += 2
-        else:
-            self.concept_map["RegressionStructures"] = [current_index]
-            current_index += 1
+        self.posible_concept_vals["RegressionStructures"] = [1]
+        self.concept_map["RegressionStructures"] = [current_index]
+        current_index += 1
         self.meta["RegressionStructures"] = self.meta.apply(
-            lambda row: (1-int(row["regression_structures"] == "absent")),
+            lambda row: (1 - int(row["regression_structures"] == "absent")),
             axis=1,
         )
 
 
-        self.posible_concept_vals["RegularDG"] = [0, 1, unc_value]
+        self.posible_concept_vals["RegularDG"] = [0, 1, 2]
         if self.use_full_concepts:
             self.concept_map["RegularDG"] = [current_index, current_index + 1, current_index + 2]
             current_index += 3
@@ -121,38 +194,28 @@ class Derm7ptDataset(object):
             self.concept_map["RegularDG"] = [current_index]
             current_index += 1
         self.meta["RegularDG"] = self.meta.apply(
-            lambda row: {"absent": 0, "regular": 1, "irregular": unc_value}[row["dots_and_globules"]],
+            lambda row: {"absent": 0, "regular": 1, "irregular": 2}[row["dots_and_globules"]],
             axis=1,
         )
 
-
-        self.posible_concept_vals["IrregularDG"] = [0, 1, unc_value]
-        if self.use_full_concepts:
-            self.concept_map["IrregularDG"] = [current_index, current_index + 1, current_index + 2]
-            current_index += 3
-        else:
-            self.concept_map["IrregularDG"] = [current_index]
-            current_index += 1
-        self.meta["IrregularDG"] = self.meta.apply(
-            lambda row: {"absent": 0, "regular": unc_value, "irregular": 1}[row["dots_and_globules"]],
-            axis=1,
-        )
-
-
-        self.posible_concept_vals["BWV"] = [0, 1]
-        if self.use_full_concepts:
-            self.concept_map["BWV"] = [current_index, current_index + 1]
-            current_index += 2
-        else:
-            self.concept_map["BWV"] = [current_index]
-            current_index += 1
+        self.posible_concept_vals["BWV"] = [1]
+        self.concept_map["BWV"] = [current_index]
+        current_index += 1
         self.meta["BWV"] = self.meta.apply(
             lambda row: {"absent": 0, "present": 1}[row["blue_whitish_veil"]],
             axis=1,
         )
+
         self.n_concepts = current_index
         self.label_map = {}
-        for val in self.meta[self.label_key].unique():
+        if label_generating_fn is not None:
+            self.label_col = self.meta.apply(
+                lambda row: label_generating_fn(row[self.label_key]),
+                axis=1,
+            )
+        else:
+            self.label_col = self.meta[self.label_key]
+        for val in self.label_col.unique():
             self.label_map[val] = len(self.label_map)
         self.meta = self.meta.iloc[indexes]
         self.transform = transform
@@ -161,17 +224,17 @@ class Derm7ptDataset(object):
         self.concepts = [
             "BWV",
             "RegularDG",
-            "IrregularDG",
+            # "IrregularDG",
             "RegressionStructures",
-            "IrregularStreaks",
+            # "IrregularStreaks",
             "RegularStreaks",
-            "AtypicalPigmentNetwork",
+            # "AtypicalPigmentNetwork",
             "TypicalPigmentNetwork",
         ]
 
 
     def num_classes(self):
-        return len(self.label_map)
+        return len(self.label_map) if len(self.label_map) > 2 else 1
 
     def _get_concepts(self, idx):
         result = []
@@ -189,7 +252,7 @@ class Derm7ptDataset(object):
 
     def _get_label(self, idx):
         return np.array(
-            self.label_map[self.meta.iloc[idx][self.label_key]]
+            self.label_map[self.label_col.iloc[idx]]
         )
 
     def __len__(self):
@@ -210,7 +273,8 @@ class Derm7ptDataset(object):
             class_label = self.label_transform(class_label)
         if self.transform:
             image = self.transform(image)
-
+        if self.num_classes() == 1:
+            class_label = float(class_label)
         concepts = self._get_concepts(idx)
         if self.concept_transform is not None:
             concepts = self.concept_transform(concepts)
@@ -228,6 +292,7 @@ def load_data(
     unc_value=0.5,
     label_key="diagnosis",
     use_full_concepts=False,
+    label_generating_fn=None,
 ):
     resized_resol = int(resol * 256/224)
     is_training = fold == "train"
@@ -255,6 +320,7 @@ def load_data(
         label_transform=label_transform,
         label_key=label_key,
         use_full_concepts=use_full_concepts,
+        label_generating_fn=label_generating_fn,
     )
     if is_training:
         drop_last = True
@@ -285,6 +351,11 @@ def generate_data(
 ):
     if root_dir is None:
         root_dir = DATASET_DIR
+    label_generating_fn = None
+    if config.get("cancer_binary_label", False):
+        label_generating_fn = lambda x: int(
+            ("clark nevus" in x)
+        )
     seed_everything(seed)
     sampling_percent = config.get("sampling_percent", 1)
     sampling_groups = config.get("sampling_groups", False)
@@ -297,6 +368,7 @@ def generate_data(
         unc_value=config.get('unc_value', 0.5),
         label_key=config.get('label_key', 'diagnosis'),
         use_full_concepts=config.get('use_full_concepts', False),
+        label_generating_fn=label_generating_fn,
     )
     n_concepts = pre_dl.dataset.n_concepts
     concept_group_map = pre_dl.dataset.concept_map.copy()
@@ -385,6 +457,7 @@ def generate_data(
         unc_value=config.get('unc_value', 0.5),
         label_key=config.get('label_key', 'diagnosis'),
         use_full_concepts=config.get('use_full_concepts', False),
+        label_generating_fn=label_generating_fn,
     )
     num_classes = og_train_dl.dataset.num_classes()
     val_size = config.get("val_size", 0.1)
@@ -440,13 +513,14 @@ def generate_data(
         unc_value=config.get('unc_value', 0.5),
         label_key=config.get('label_key', 'diagnosis'),
         use_full_concepts=config.get('use_full_concepts', False),
+        label_generating_fn=label_generating_fn,
     )
 
     # Finally, determine whether or not we will need to compute the imbalance factors
     if config.get('weight_loss', False):
         attribute_count = np.zeros((n_concepts,))
         samples_seen = 0
-        for i, (_, (y, c)) in enumerate(train_dl):
+        for i, (_, y, c) in enumerate(train_dl):
             c = c.cpu().detach().numpy()
             attribute_count += np.sum(c, axis=0)
             samples_seen += c.shape[0]

cem/models/cbm.py
@@ -48,7 +48,7 @@ def compute_bin_accuracy(c_pred, y_pred, c_true, y_true):
     c_f1 = c_f1/num_seen
     y_accuracy = sklearn.metrics.accuracy_score(y_true, y_pred)
     if len(np.unique(y_true)) == 1:
-        y_auc = sklearn.metrics.accuracy_score(y_true,  y_probs)
+        y_auc = sklearn.metrics.accuracy_score(y_true,  y_pred)
     else:
         y_auc = sklearn.metrics.roc_auc_score(y_true, y_probs)
     y_f1 = sklearn.metrics.f1_score(y_true, y_pred)

cem/train/training.py
@@ -5,6 +5,7 @@ import os
 import pytorch_lightning as pl
 import torch
 import logging
+import time
 
 from pytorch_lightning import seed_everything
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
@@ -689,9 +690,20 @@ def train_model(
                 # Then we simply load the model and proceed
                 print("\tFound cached model... loading it")
                 model.load_state_dict(torch.load(model_saved_path))
+                if os.path.exists(
+                    model_saved_path.replace(".pt", "_training_times.npy")
+                ):
+                    [training_time, num_epochs] = np.load(
+                        model_saved_path.replace(".pt", "_training_times.npy"),
+                    )
+                else:
+                    training_time, num_epochs = 0, 0
             else:
                 # Else it is time to train it
+                start_time = time.time()
                 fit_trainer.fit(model, train_dl, val_dl)
+                num_epochs = fit_trainer.current_epoch
+                training_time = time.time() - start_time
                 config_copy = copy.deepcopy(config)
                 if "c_extractor_arch" in config_copy and (
                     not isinstance(config_copy["c_extractor_arch"], str)
@@ -708,6 +720,10 @@ def train_model(
                     torch.save(
                         model.state_dict(),
                         model_saved_path,
+                    )
+                    np.save(
+                        model_saved_path.replace(".pt", "_training_times.npy"),
+                        np.array([training_time, num_epochs]),
                     )
             # freeze model and compute test accuracy
             if test_dl is not None:
@@ -761,11 +777,14 @@ def train_model(
                     key: val
                     for (key, val) in zip(keys, values)
                 }
+                test_results['training_time'] = training_time
+                test_results['num_epochs'] = num_epochs
                 print(
                     f'c_acc: {test_results["test_acc_c"]*100:.2f}%, '
                     f'y_acc: {test_results["test_acc_y"]*100:.2f}%, '
                     f'c_auc: {test_results["test_auc_c"]*100:.2f}%, '
-                    f'y_auc: {test_results["test_auc_y"]*100:.2f}%'
+                    f'y_auc: {test_results["test_auc_y"]*100:.2f}% with '
+                    f'{num_epochs} epochs in {training_time:.2f} seconds'
                 )
             else:
                 test_results = None
@@ -819,14 +838,30 @@ def train_model(
             # Then we simply load the model and proceed
             print("\tFound cached model... loading it")
             model.load_state_dict(torch.load(model_saved_path))
+            if os.path.exists(
+                model_saved_path.replace(".pt", "_training_times.npy")
+            ):
+                [training_time, num_epochs] = np.load(
+                    model_saved_path.replace(".pt", "_training_times.npy"),
+                )
+            else:
+                training_time, num_epochs = 0, 0
         else:
             # Else it is time to train it
+            start_time = time.time()
             fit_trainer.fit(model, train_dl, val_dl)
+            training_time = time.time() - start_time
+            num_epochs = fit_trainer.current_epoch
             if save_model and (result_dir is not None):
                 torch.save(
                     model.state_dict(),
                     model_saved_path,
                 )
+                np.save(
+                    model_saved_path.replace(".pt", "_training_times.npy"),
+                    np.array([training_time, num_epochs]),
+                )
+
 
         if not os.path.exists(os.path.join(
             result_dir,
@@ -893,11 +928,14 @@ def train_model(
                 key: val
                 for (key, val) in zip(keys, values)
             }
+            test_results['training_time'] = training_time
+            test_results['num_epochs'] = num_epochs
             print(
                 f'c_acc: {test_results["test_acc_c"]*100:.2f}%, '
                 f'y_acc: {test_results["test_acc_y"]*100:.2f}%, '
                 f'c_auc: {test_results["test_auc_c"]*100:.2f}%, '
-                f'y_auc: {test_results["test_auc_y"]*100:.2f}%'
+                f'y_auc: {test_results["test_auc_y"]*100:.2f}% with '
+                f'{num_epochs} epochs in {training_time:.2f} seconds'
             )
         else:
             test_results = None

experiments/new_run_experiments.py
@@ -239,6 +239,9 @@ def main(
     task_class_weights = None
 
     if experiment_config['shared_params'].get('use_task_class_weights', False):
+        logging.info(
+            f"Computing task class weights in the training dataset with size {len(train_dl)}..."
+        )
         attribute_count = np.zeros((max(n_tasks, 2),))
         samples_seen = 0
         for i, data in enumerate(train_dl):
@@ -690,7 +693,7 @@ if __name__ == '__main__':
                 torch.nn.Linear(128, output_dim),
             ])
         loaded_config["c_extractor_arch"] = synth_c_extractor_arch
-    elif args.dataset == "mnist_add":
+    elif loaded_config["dataset"] == "mnist_add":
         data_module = mnist_data_module
         args.project_name = args.project_name.format(ds_name=args.dataset)
         utils.extend_with_global_params(