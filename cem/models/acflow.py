f9ffe3eacb803578e4a91efee396f548a189d83d
Thomas Yuan <thomasyuan1@gmail.com>
2024-02-04 15:42:30 +0000

    fix

--- cem/models/acflow.py
+++ cem/models/acflow.py
@@ -42,9 +42,7 @@ class ACFlow(pl.LightningModule):
                 y = torch.randint(0, self.n_concepts, [B*N])
                 forward = False
         if(y.shape != (B*N)):
-            import pdb
-            pdb.set_trace()
-            if(y.shape == (B)):
+            if(y.shape == (B,)):
                 y = torch.tile(torch.unsqueeze(y, dim = 1), [1, N])
                 y = torch.reshape(y, [B * N])
             elif(y.shape == (B, N)):
