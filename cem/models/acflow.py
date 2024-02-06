22e81930d15befad8d42f8ff0ff6f492ae05e7f8
Thomas Yuan <thomasyuan1@gmail.com>
2024-02-06 14:39:51 +0000

    debug

--- cem/models/acflow.py
+++ cem/models/acflow.py
@@ -440,6 +440,8 @@ class LULinear(BaseTransform):
         t = torch.gather(t, 1, order)
 
         weight = torch.matmul(torch.matmul(t, weight), torch.permute(t, (0,2,1)))
+        import pdb
+        pdb.set_trace()
         bias = torch.squeeze(torch.matmul(t, torch.unsqueeze(bias, dim = 1)), dim = -1)
         
         return weight, bias
